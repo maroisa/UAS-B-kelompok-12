c8d8cd2d77aeb5d188e000d195d6a472a093453a
maroisa <ahmadroisul412@gmail.com>
2025-06-23 10:38:08 +0700

    kalkulasi profit

--- core/kalkulator.py
+++ core/kalkulator.py
@@ -1,17 +1,12 @@
 from core import utils
 
-from classes.kue_kering.butter_cookies import ButterCookies
-from classes.kue_kering.muffin import Muffin
-from classes.roti.croissant import Croissant
-from classes.roti.roti_manis import RotiManis
+def input_produk(produk):
+    jumlah = utils.input_int("Masukkan jumlah produk (pcs): ")
+    profit = hitung_profit(produk)
+    print("Biaya Produksi:", produk.biaya_produksi)
+    print("Harga Jual:", produk.harga_jual)
+    print("Profit:", profit)
+    print(f"Total profit ({jumlah} pcs):", profit * jumlah)
 
-def input_produk():
-    print("=== Tambah Produk Roti ===")
-    print("1. Roti Manis")
-    print("2. Croissant")
-    print("3. Butter Cookies")
-    print("4. Muffin")
-
-    jawaban = utils.input_pilihan("Pilih jenis produk (1-4): ", 4)
-    jumlah = utils.input_int("Jumlah produk (pcs): ")
-    
+def hitung_profit(produk):
+    return produk.harga_jual - produk.biaya_produksi
