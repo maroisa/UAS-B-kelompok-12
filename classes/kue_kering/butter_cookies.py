4a62e1e1a99861a9bf99e0e356ad0f0ca4b4bbef
Ahmad Roisul <126882676+maroisa@users.noreply.github.com>
2025-06-24 08:21:09 +0700

    mempercepat sleep ke 1 detik

--- classes/kue_kering/butter_cookies.py
+++ classes/kue_kering/butter_cookies.py
@@ -8,19 +8,19 @@ class ButterCookies(KueKering):
         print("Menyiapkan bahan")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("Mencampur bahan.")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("Mendiamkan adonan di kulkas.")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
        
@@ -28,31 +28,31 @@ class ButterCookies(KueKering):
         print("Memanaskan oven pada suhu 160–180°C.")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("Menyiapkan loyang yang diberi alas dengan kertas baking/parchment.")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("membentuk adonan dan menatanya di atas loyang.")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("Memanggang cookies hingga pinggiran cookies berwarna keemasan.")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("Mengeluarkan cookies dari oven")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
 
@@ -60,19 +60,19 @@ class ButterCookies(KueKering):
         print("mendinginkan cookies di rak kawat.")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("Menyiapkan topping")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
         print("Memberikan hiasan dengan topping")
         print("Sedang dalam proses", end='', flush=True)
         for _ in range(3):  
-            time.sleep(2)
+            time.sleep(1)
             print('.', end='', flush=True)
         print()
 
