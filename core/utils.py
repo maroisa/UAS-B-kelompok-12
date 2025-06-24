99c64d5cf730e304b424aee5fd7c1f6facb20dc9
Nadhif62 <nadhifathaasyamn@gmail.com>
2025-06-24 09:05:22 +0700

    Update Utils

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,13 @@
 from rich.prompt import Prompt
+from rich.console import Console
 
+console = Console()
 def input_pilihan(message: str, pilihanNum: int):
     while True:
         pilihan = input_int(message)
         if pilihan in range(1, pilihanNum + 1): return pilihan
         else:
-            print(f"Pilihan tidak valid. Harap masukkan salah satu dari {tuple(range(1, pilihanNum + 1))}.",style="#CD5C5C")
+            console.print(f"Pilihan tidak valid. Harap masukkan salah satu dari {tuple(range(1, pilihanNum + 1))}.",style="#CD5C5C")
 
 
 def input_int(message: str):
@@ -15,7 +17,7 @@ def input_int(message: str):
             nilai = int(nilai)
             return nilai
         except: 
-            print("Input harus berupa angka!",style="#CD5C5C")
+            console.print("Input harus berupa angka!",style="#CD5C5C")
             continue
 
 
