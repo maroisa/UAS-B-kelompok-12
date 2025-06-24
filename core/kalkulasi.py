6ac55e7d9101e544bd21792e6d59b39cdb0d288d
maroisa <ahmadroisul412@gmail.com>
2025-06-24 08:10:15 +0700

    styling mata uang di kalkulasi.py

--- core/kalkulasi.py
+++ core/kalkulasi.py
@@ -1,16 +1,34 @@
+import time
+from rich.console import Console
 
 from core import utils
 from classes.kue_kering.kue_kering import KueKering
 from interfaces.pengembangan import Pengembangan
 
+console = Console()
+
 # Kalkulator
 def input_produk(produk):
     jumlah = utils.input_int("Masukkan jumlah produk (pcs)")
     profit = hitung_profit(produk)
-    print("Biaya Produksi:", produk.biaya_produksi)
-    print("Harga Jual:", produk.harga_jual)
-    print("Profit:", profit)
-    print(f"Total profit ({jumlah} pcs):", profit * jumlah)
+    console.print(
+        "[#87CEFA]Biaya Produksi:[/#87CEFA]", 
+        format_rupiah(produk.biaya_produksi), highlight=False
+    )
+    console.print(
+        "[#87CEFA]Harga Jual:[/#87CEFA]", format_rupiah(produk.harga_jual), 
+        highlight=False
+    )
+    console.print(
+        "[#87CEFA]Profit:[/#87CEFA]", format_rupiah(profit), 
+        highlight=False
+    )
+    console.print(
+        f"[#87CEFA]Total profit ({jumlah} pcs):[/#87CEFA]", 
+        format_rupiah(profit * jumlah), highlight=False
+    )
+
+    time.sleep(2)
 
 def hitung_profit(produk):
     return produk.harga_jual - produk.biaya_produksi
@@ -29,3 +47,6 @@ def simulasi(produk):
         produk.topping()
     
     print(f"\n\n=== {produk.nama.upper()} SUDAH SIAP DISAJIKAN! ===\n")
+
+def format_rupiah(angka):
+    return f"Rp{int(angka):,}".replace(",", ".")
